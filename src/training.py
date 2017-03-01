8e41a6ffff052f0df592abcc5b6ad2a6c802106e
Chaoyue Wang <wcy940418@gmail.com>
2017-03-01 11:55:44 -0500

    Add files via upload

--- src/training.py
+++ src/training.py
@@ -1,5 +1,6 @@
 import model
 import dataset
+import os
 
 class Conf:
 	self.nClasses = 36
@@ -9,23 +10,25 @@ class Conf:
 	self.displayInterval = 200
 	self.testInteval = 100
 
-
-gConfig = Conf()
-sess = tf.Session()
-
-imgs = tf.placeholder(tf.float32, [None, 32, 100, 1])
-labels = tf.sparse_placeholder(tf.int32)
-batches = tf.placeholder(tf.int32, [None])
-
-crnn = CRNN(imgs, gConfig, None, sess)
-ctc = CtcCriterion(crnn.prob, labels, batches)
-optimizer = tf.train.AdadeltaOptimizer(0.001).minimize(ctc.cost)
-data = DatasetLmdb('../data')
-
-init = tf.initialize_all_variables()
-sess.run(init)
-
 if __name__ == '__main__':
+	modelFiles = os.listdir("./model")
+	parameterFile = None
+	for file in modelFiles:
+		if file == 
+	gConfig = Conf()
+	sess = tf.Session()
+
+	imgs = tf.placeholder(tf.float32, [None, 32, 100, 1])
+	labels = tf.sparse_placeholder(tf.int32)
+	batches = tf.placeholder(tf.int32, [None])
+
+	crnn = CRNN(imgs, gConfig, None, sess)
+	ctc = CtcCriterion(crnn.prob, labels, batches)
+	optimizer = tf.train.AdadeltaOptimizer(0.001).minimize(ctc.cost)
+	data = DatasetLmdb('../data')
+
+	init = tf.initialize_all_variables()
+	sess.run(init)
 	trainAccuracy = 0
 	for i in range(gConfig.maxIteration):
 		if i % gConfig.testInteval == 0:
