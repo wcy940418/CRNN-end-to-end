8eab006b3cf43930d2491925647e50c02b016133
Chaoyue Wang <wcy940418@gmail.com>
2017-03-02 01:10:56 -0500

    Repaired create_dataset.py

--- tool/create_dataset.py
+++ tool/create_dataset.py
@@ -1,71 +1,83 @@
-import os
+import os, sys
 import lmdb
 import cv2
-
+import numpy as np
+import base64
 
 def checkImageIsValid(imageBin):
-    if imageBin is None:
-        return False
-    imageBuf = np.fromstring(imgBin, dtype=np.uint8)
-    img = cv2.imdecode(imgBuf, cv2.IMREAD_GRAYSCALE)
-    imgH, imgW = img.shape[0], img.shape[1]
-    if imgH * imgW == 0:
-        return False
-    return True
+	if imageBin is None:
+		return False
+	imageBuf = np.fromstring(imageBin, dtype=np.uint8)
+	img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
+	imgH, imgW = img.shape[0], img.shape[1]
+	if imgH * imgW == 0:
+		return False
+	return True
 
 
 def writeCache(env, cache):
-    with env.begin(write=True) as txn:
-        for k, v in cache.iteritems():
-            txn.put(k, v)
+	with env.begin(write=True) as txn:
+		for k, v in cache.iteritems():
+			txn.put(k, v)
 
 
 def createDataset(outputPath, imagePathList, labelList, lexiconList=None, checkValid=True):
-    """
-    Create LMDB dataset for CRNN training.
+	"""
+	Create LMDB dataset for CRNN training.
 
-    ARGS:
-        outputPath    : LMDB output path
-        imagePathList : list of image path
-        labelList     : list of corresponding groundtruth texts
-        lexiconList   : (optional) list of lexicon lists
-        checkValid    : if true, check the validity of every image
-    """
-    assert(len(imagePathList) == len(labelList))
-    nSamples = len(imagePathList)
-    env = lmdb.open(outputPath, map_size=1099511627776)
-    cache = {}
-    cnt = 1
-    for i in xrange(nSamples):
-        imagePath = imagePathList[i]
-        label = labelList[i]
-        if not os.path.exists(imagePath):
-            print('%s does not exist' % imagePath)
-            continue
-        with open(imagePath, 'r') as f:
-            imageBin = f.read()
-        if checkValid:
-            if not checkImageIsValid(imageBin):
-                print('%s is not a valid image' % imagePath)
-                continue
+	ARGS:
+		outputPath    : LMDB output path
+		imagePathList : list of image path
+		labelList     : list of corresponding groundtruth texts
+		lexiconList   : (optional) list of lexicon lists
+		checkValid    : if true, check the validity of every image
+	"""
+	assert(len(imagePathList) == len(labelList))
+	nSamples = len(imagePathList)
+	env = lmdb.open(outputPath, map_size=1099511627776)
+	cache = {}
+	cnt = 1
+	for i in xrange(nSamples):
+		imagePath = imagePathList[i]
+		label = labelList[i]
+		if not os.path.exists(imagePath):
+			print('%s does not exist' % imagePath)
+			continue
+		with open(imagePath, 'r') as f:
+			imageBin = f.read()
+		if checkValid:
+			if not checkImageIsValid(imageBin):
+				print('%s is not a valid image' % imagePath)
+				continue
 
-        imageKey = 'image-%09d' % cnt
-        labelKey = 'label-%09d' % cnt
-        cache[imageKey] = imageBin
-        cache[labelKey] = label
-        if lexiconList:
-            lexiconKey = 'lexicon-%09d' % cnt
-            cache[lexiconKey] = ' '.join(lexiconList[i])
-        if cnt % 1000 = 0:
-            writeCache(env, cache)
-            cache = {}
-            print('Written %d / %d' % (cnt, nSamples))
-        cnt += 1
-    nSamples = cnt-1
-    cache['num-samples'] = nSamples
-    writeCache(env, cache)
-    print('Created dataset with %d samples' % nSamples)
+		imageKey = 'image-%09d' % cnt
+		labelKey = 'label-%09d' % cnt
+		cache[imageKey] = imageBin
+		cache[labelKey] = label
+		if lexiconList:
+			lexiconKey = 'lexicon-%09d' % cnt
+			cache[lexiconKey] = ' '.join(lexiconList[i])
+		if cnt % 1000 == 0:
+			writeCache(env, cache)
+			cache = {}
+			print('Written %d / %d' % (cnt, nSamples))
+		cnt += 1
+	nSamples = cnt-1
+	cache['num-samples'] = str(nSamples)
+	writeCache(env, cache)
+	print('Created dataset with %d samples' % nSamples)
 
 
 if __name__ == '__main__':
-    pass
+	'''ro run create_dataset, use command: 
+	"python create_dataset.py <config text file path>" '''
+	configFile = sys.argv[1]
+	outputPath = configFile.split('.')[0] + 'data'
+	imageList = []
+	labelList = []
+	with open(configFile, 'r') as f:
+		for line in f:
+			image, label = line.strip().split(' ')
+			imageList.append('./'+image)
+			labelList.append(label)
+	createDataset(outputPath, imageList, labelList)
