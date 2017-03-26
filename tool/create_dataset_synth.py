77fd699dda4ad9a0e07b5ceaddd3f3d8fe162cec
Chaoyue Wang <wcy940418@gmail.com>
2017-03-26 11:44:04 -0400

    Added image checker

--- tool/create_dataset_synth.py
+++ tool/create_dataset_synth.py
@@ -3,13 +3,22 @@ import lmdb
 import numpy as np
 import base64
 
+def checkImageIsValid(imagePath):
+	if imageBin is None:
+		return False
+	img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+	imgH, imgW = img.shape[0], img.shape[1]
+	if imgH * imgW == 0:
+		return False
+	return True
+
 def writeCache(env, cache):
 	with env.begin(write=True) as txn:
 		for k, v in cache.iteritems():
 			txn.put(k, v)
 
 
-def createDataset(outputPath, configFile):
+def createDataset(outputPath, configFile, imgDir):
 	"""
 	Create LMDB dataset for CRNN training.
 
@@ -26,13 +35,15 @@ def createDataset(outputPath, configFile):
 	with open(configFile, 'r') as f:
 		for line in f:
 			image_path = line.strip().split(' ')[0]
-			key = "%08d" % cnt
-			cache[key] = image_path
-			if cnt % 10000 == 0 and cnt != 0:
-				writeCache(env, cache)
-				cache = {}
-				print "Written %d images" % cnt
-			cnt += 1
+			img = os.path.join(imgDir, image_path)
+			if checkImageIsValid(img) :
+				key = "%08d" % cnt
+				cache[key] = image_path
+				if cnt % 10000 == 0 and cnt != 0:
+					writeCache(env, cache)
+					cache = {}
+					print "Written %d images" % cnt
+				cnt += 1
 	nSamples = cnt-1
 	cache['num-samples'] = str(nSamples)
 	writeCache(env, cache)
@@ -41,7 +52,8 @@ def createDataset(outputPath, configFile):
 
 if __name__ == '__main__':
 	'''ro run create_dataset, use command: 
-	"python create_dataset.py <config text file path>" '''
+	"python create_dataset.py <config text file path> <image files dir path>" '''
 	configFile = sys.argv[1]
-	outputPath = configFile.split('.')[0] + '_data'
-	createDataset(outputPath, configFile)
+	imgDir = sys.argv[2]
+	outputPath = './data'
+	createDataset(outputPath, configFile, imgDir)
